f515062bd1dfe211ee02c0e19b896dff21b46762
Jonathan Bartlett <jonathan@bartlettpublishing.com>
2023-11-24 15:22:22 -0600

    Added logging; added some options

--- simplechat.rb
+++ simplechat.rb
@@ -2,6 +2,7 @@
 
 require "httparty"
 require "json"
+require "optparse"
 
 CHAT_URL = "https://api.openai.com/v1/chat/completions"
 headers = {
@@ -12,10 +13,25 @@ headers = {
 # Models - gpt-4, gpt-3.5-turbo
 # gpt-4-1106-preview (GPT-4 Turbo)
 
+model = "gpt-4"
+system_message = "You are a helpful assistant."
+logdir = ""
+
+OptionParser.new do |parser|
+	parser.on("-m", "--model MODELNAME", "specifies a specific model to use") do |m|
+		model = m
+	end
+	parser.on("-l", "--logidr DIR", "specifies the log directory") do |l|
+		logdir = l
+	end
+	parser.on("-s", "--system MESSAGE", "starts with a system message") do |s|
+		system_message = s
+	end
+end.parse!
+
 structure = {
-	:model => "gpt-4",
+	:model => model,
 }
-system_message = "You are a helpful assistan."
 
 messages = [
 	{
@@ -27,12 +43,26 @@ messages = [
 promptcount = 1
 print "#{promptcount}) "
 
+
+def logreq(logdir, url, req, resp)
+	return if logdir == nil || logdir == ""
+	t = Time.now.to_i
+	File.open("#{logdir}/chat_#{t}.log", "w+") do |fh|
+		fh.puts(url)
+		fh.puts(req)
+		fh.puts(resp)
+	end
+end
+
+
+
 ARGF.each_line do |line|
 	# Perform the request
 	messages.push(:role => :user, :content => line)
 	req = structure.merge(:messages => messages)
 	resp = HTTParty.post(CHAT_URL, :body => req.to_json, :headers => headers)
 	result = JSON.parse(resp.body)
+	logreq(logdir, CHAT_URL, req, result.to_json)
 
 	# Show the user the results
 	result["choices"].each do |choice|
